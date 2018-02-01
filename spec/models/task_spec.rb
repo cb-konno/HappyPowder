1baf40339c08c56cad084ee4129a0159844a53b1
Sumiya Konno <s-konno@cookbiz.jp>
2018-02-01 17:56:15 +0900

    feature spec追加

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,28 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe 'リンクのテスト', type: :feature do
-
-  it 'トップページから新規作成ページに遷移する' do
-    visit root_path
-    click_link '新規作成'
-    expect(page).to have_content 'タスク新規作成'
-  end
-
-  it '新規作成ページからトップページに遷移する' do
-    visit new_task_path
-    click_link '一覧'
-    expect(page).to have_content 'タスク一覧'
-  end
-end
-
-RSpec.describe 'タスクの登録・更新・削除のテスト', type: :feature do
-
-  it 'タスクの登録をテストします' do
-    visit new_task_path
-    fill_in 'task[name]', with: 'テストで追加する課題の名前'
-    fill_in 'task[description]', with: '課題の説明文を長々と…'
-    click_button 'Create Task'
-    expect(page).to have_content 'New Task Created.'
-  end
+  #model
 
 end
