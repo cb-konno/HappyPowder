74f4c7928aeb552637f70a0fc73020001c0494fa
Sumiya Konno <s-konno@cookbiz.jp>
2018-02-08 18:09:04 +0900

    テストを修正

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
-def parse_data(included_th: true)
+def parse_data
   row_count = page.find('table').all('tr').count
   col_count = page.find('table').all("th").count
   all_td = page.find('table').all("td").map { |e| e.text() }
 
   table = []
-  row_count -= 1 unless included_th
-
-  for row in 0..(row_count - 1)
-    table << all_td.shift(col_count)
-  end
+  row_count.times { table << all_td.shift(col_count) }
   table
 end
