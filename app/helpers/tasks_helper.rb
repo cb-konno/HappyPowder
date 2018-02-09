b35b84a8f03454535ca570c2338652669a3d5a86
Sumiya Konno <s-konno@cookbiz.jp>
2018-02-09 16:38:50 +0900

    セレクトボックスの長いところ修正

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,17 @@
 module TasksHelper
+  def status_selection
+    options = [
+      [],
+      [t('task.status.created'), 'created'],
+      [t('task.status.doing'), 'doing'],
+      [t('task.status.done'), 'done']]
+  end
+
+  def priority_selection
+    options = [
+      [],
+      [t('task.priority.high'), 'high'],
+      [t('task.priority.middle'), 'middle'],
+      [t('task.priority.low'), 'low']]
+  end
 end
