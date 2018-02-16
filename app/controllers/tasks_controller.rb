3c242c5efb1d801f0dc5afaf3c19bb3f68a3b244
Sumiya Konno <s-konno@cookbiz.jp>
2018-02-16 17:32:27 +0900

    ransack_with_check_paramsのテスト追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
 
   def index
     @q = Task.ransack_with_check_params(params)
-    @tasks = @q.result
+    p @tasks = @q.result
     @page_title = t('title_index', title: Task.model_name.human)
   end
 
